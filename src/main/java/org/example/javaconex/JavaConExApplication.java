35ec432d4cb2d5b9c5cb91dde6a61b867130d4c9
jmartter <150058251+jmartter@users.noreply.github.com>
2024-10-10 11:52:03 +0200

    Initial commit

---
+++ src/main/java/org/example/javaconex/JavaConExApplication.java
@@ -0,0 +1,13 @@
+package org.example.javaconex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaConExApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaConExApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/javaconex/JavaConExApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.javaconex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaConExApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
