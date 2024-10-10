963a6f339e64a5632d110683458630dba0028bd9
HugoSanchezGallego <hsancgal@myuax.com>
2024-10-10 18:59:20 +0200

    Soy la hostia

--- src/main/java/org/example/javaconex/ImplementacionBase/ValorDataService.java
+++ src/main/java/org/example/javaconex/ImplementacionBase/ValorDataService.java
@@ -2,6 +2,8 @@ package org.example.javaconex.ImplementacionBase;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
--- src/main/java/org/example/javaconex/JavaConExApplication.java
+++ src/main/java/org/example/javaconex/JavaConExApplication.java
@@ -5,19 +5,46 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import jakarta.annotation.PostConstruct;
+import java.io.IOException;
+
 @SpringBootApplication
 public class JavaConExApplication {
 
-    private static ValorDataService valorDataService;
+    private final ValorDataService valorDataService;
 
     @Autowired
     public JavaConExApplication(ValorDataService valorDataService) {
-        JavaConExApplication.valorDataService = valorDataService;
+        this.valorDataService = valorDataService;
     }
 
     public static void main(String[] args) {
         SpringApplication.run(JavaConExApplication.class, args);
-        valorDataService.loadCSVToDatabase("src/main/resources/valores_normales.csv");
     }
 
+    @PostConstruct
+    public void init() {
+        valorDataService.loadCSVToDatabase("src/main/resources/valores_normales.csv");
+        openBrowser();
+    }
+
+    private void openBrowser() {
+        String url = "http://localhost:8080/menu.html";
+        String os = System.getProperty("os.name").toLowerCase();
+        Runtime runtime = Runtime.getRuntime();
+
+        try {
+            if (os.contains("win")) {
+                runtime.exec("rundll32 url.dll,FileProtocolHandler " + url);
+            } else if (os.contains("mac")) {
+                runtime.exec("open " + url);
+            } else if (os.contains("nix") || os.contains("nux")) {
+                runtime.exec("xdg-open " + url);
+            } else {
+                System.err.println("Unsupported OS. Please open the following URL manually: " + url);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
